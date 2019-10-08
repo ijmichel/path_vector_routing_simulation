76c9840fe51f9bbda5665d6b57e9096f42eaa6ef
ian.michel@gmail.com <Reynolds307!5>
2019-10-07 20:24:57 -0400

    Initial Commit 2

---
+++ cmake-build-debug/CMakeFiles/manager_send.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/manager_send.dir/manager_send.c.o"
+  "manager_send.pdb"
+  "manager_send"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/manager_send.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
