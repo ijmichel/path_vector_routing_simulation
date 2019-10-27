f0774f0b89917cd7cdba1efca09969055ddf85d4
ian.michel@gmail.com <Reynolds307!5>
2019-10-26 23:23:33 -0400

    Workspace and executable push

---
+++ cmake-build-release/CMakeFiles/manager_send.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/manager_send.dir/manager_send.c.o"
+  "manager_send"
+  "manager_send.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/manager_send.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
